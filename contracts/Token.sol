d8b873a7b569a365ab74625ea805e0710f1d74ec
CryptoThibault <thibault.chalaoux@gmail.com>
2021-07-28 20:52:41 +0200

    token factory

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,4 +2,15 @@
 
 pragma solidity ^0.8.5;
 
-contract Token {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        address owner,
+        uint256 supply
+    ) ERC20(name, symbol) {
+        _mint(owner, supply);
+    }
+}
